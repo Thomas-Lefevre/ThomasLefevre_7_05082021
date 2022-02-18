68c6569c84ca24c94cee9e112b20f7710b8dd965
Thomas-Lefevre <thomas.lefevre4652@gmail.com>
2022-02-18 15:27:57 +0100

    correction responsive tags

--- public/js/filterList.js
+++ public/js/filterList.js
@@ -27,10 +27,14 @@ const apparatusInput = document.querySelector('#appareil');
 
 ustensilsForm.addEventListener("click", () => {
     if (ustensilsList.classList.contains("rechercheUstensilesAside--inactive")) {
-        ustensilsForm.style.width = "50%";
-        apparatusForm.style.width = "20%";
-        ingredientsForm.style.width = "20%";
-        researchTagList.style.width = "100%";
+        if (window.innerWidth <= 700) {
+
+        } else if (701 <= window.innerWidth) {
+            ustensilsForm.style.width = "50%";
+            apparatusForm.style.width = "20%";
+            ingredientsForm.style.width = "20%";
+            researchTagList.style.width = "100%";
+        }
         chevronApparatus.classList.replace("fa-angle-up", "fa-angle-down");
         apparatusList.classList.replace("rechercheAppareilAside--active", "rechercheAppareilAside--inactive");
         chevronIngredients.classList.replace("fa-angle-up", "fa-angle-down");
@@ -38,10 +42,19 @@ ustensilsForm.addEventListener("click", () => {
         chevronUstensils.classList.replace("fa-angle-down", "fa-angle-up");
         ustensilsList.classList.replace("rechercheUstensilesAside--inactive", "rechercheUstensilesAside--active");
     } else {
-        ustensilsForm.style.width = "30%";
-        apparatusForm.style.width = "30%";
-        ingredientsForm.style.width = "30%";
-        researchTagList.style.width = "50%";
+        if (window.innerWidth <= 700) {
+
+        } else if (701 <= window.innerWidth <= 1300) {
+            ustensilsForm.style.width = "30%";
+            apparatusForm.style.width = "30%";
+            ingredientsForm.style.width = "30%";
+            researchTagList.style.width = "75%";
+        } else if (window.innerWidth >= 1301) {
+            ustensilsForm.style.width = "30%";
+            apparatusForm.style.width = "30%";
+            ingredientsForm.style.width = "30%";
+            researchTagList.style.width = "50%";
+        }
         chevronUstensils.classList.replace("fa-angle-up", "fa-angle-down");
         ustensilsList.classList.replace("rechercheUstensilesAside--active", "rechercheUstensilesAside--inactive");
     }
@@ -49,10 +62,14 @@ ustensilsForm.addEventListener("click", () => {
 
 apparatusForm.addEventListener("click", () => {
     if (apparatusList.classList.contains("rechercheAppareilAside--inactive")) {
-        apparatusForm.style.width = "50%";
-        ingredientsForm.style.width = "20%";
-        ustensilsForm.style.width = "20%";
-        researchTagList.style.width = "100%";
+        if (window.innerWidth <= 700) {
+
+        } else if (701 <= window.innerWidth) {
+            apparatusForm.style.width = "50%";
+            ingredientsForm.style.width = "20%";
+            ustensilsForm.style.width = "20%";
+            researchTagList.style.width = "100%";
+        }
         chevronApparatus.classList.replace("fa-angle-down", "fa-angle-up");
         apparatusList.classList.replace("rechercheAppareilAside--inactive", "rechercheAppareilAside--active");
         chevronIngredients.classList.replace("fa-angle-up", "fa-angle-down");
@@ -60,10 +77,19 @@ apparatusForm.addEventListener("click", () => {
         chevronUstensils.classList.replace("fa-angle-up", "fa-angle-down");
         ustensilsList.classList.replace("rechercheUstensilesAside--active", "rechercheUstensilesAside--inactive");
     } else {
-        apparatusForm.style.width = "30%";
-        ingredientsForm.style.width = "30%";
-        ustensilsForm.style.width = "30%";
-        researchTagList.style.width = "50%";
+        if (window.innerWidth <= 700) {
+
+        } else if (701 <= window.innerWidth <= 1300) {
+            ustensilsForm.style.width = "30%";
+            apparatusForm.style.width = "30%";
+            ingredientsForm.style.width = "30%";
+            researchTagList.style.width = "75%";
+        } else if (window.innerWidth >= 1301) {
+            apparatusForm.style.width = "30%";
+            ingredientsForm.style.width = "30%";
+            ustensilsForm.style.width = "30%";
+            researchTagList.style.width = "50%";
+        }
         chevronApparatus.classList.replace("fa-angle-up", "fa-angle-down");
         apparatusList.classList.replace("rechercheAppareilAside--active", "rechercheAppareilAside--inactive");
     }
@@ -71,10 +97,14 @@ apparatusForm.addEventListener("click", () => {
 
 ingredientsForm.addEventListener("click", () => {
     if (ingredientsList.classList.contains("rechercheIngredientAside--inactive")) {
-        ingredientsForm.style.width = "50%";
-        apparatusForm.style.width = "20%";
-        ustensilsForm.style.width = "20%";
-        researchTagList.style.width = "100%";
+        if (window.innerWidth <= 700) {
+
+        } else if (701 <= window.innerWidth) {
+            ingredientsForm.style.width = "50%";
+            apparatusForm.style.width = "20%";
+            ustensilsForm.style.width = "20%";
+            researchTagList.style.width = "100%";
+            }
         chevronApparatus.classList.replace("fa-angle-up", "fa-angle-down");
         apparatusList.classList.replace("rechercheAppareilAside--active", "rechercheAppareilAside--inactive");
         chevronIngredients.classList.replace("fa-angle-down", "fa-angle-up");
@@ -82,10 +112,19 @@ ingredientsForm.addEventListener("click", () => {
         chevronUstensils.classList.replace("fa-angle-up", "fa-angle-down");
         ustensilsList.classList.replace("rechercheUstensilesAside--active", "rechercheUstensilesAside--inactive");
     } else {
-        ingredientsForm.style.width = "30%";
-        apparatusForm.style.width = "30%";
-        ustensilsForm.style.width = "30%"
-        researchTagList.style.width = "50%";
+        if (window.innerWidth <= 700) {
+
+        } else if (701 <= window.innerWidth <= 1300) {
+            ustensilsForm.style.width = "30%";
+            apparatusForm.style.width = "30%";
+            ingredientsForm.style.width = "30%";
+            researchTagList.style.width = "75%";
+        } else if (window.innerWidth >= 1301) {
+            apparatusForm.style.width = "30%";
+            ingredientsForm.style.width = "30%";
+            ustensilsForm.style.width = "30%";
+            researchTagList.style.width = "50%";
+        }
         chevronIngredients.classList.replace("fa-angle-up", "fa-angle-down");
         ingredientsList.classList.replace("rechercheIngredientAside--active", "rechercheIngredientAside--inactive");
     }
